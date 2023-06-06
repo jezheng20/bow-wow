010f163af46f2cf226ee85a2d87c12487e7ef9ec
Zheng, Jeffrey H <jeffreyzheng505@gmail.com>
2023-06-06 10:46:38 -0400

    Add splashscreen
    Bow wow text

---
+++ app/src/main/java/com/example/bowwow/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.example.bowwow
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        var ivSplash = findViewById<ImageView>(R.id.ivSplash)
+        ivSplash.alpha = 0f
+        ivSplash.animate().setDuration(1500).alpha(1f).withEndAction {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
+    }
+}
